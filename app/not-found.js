3a7879885d9ad01625e360d5a2ec370bbb86613a
Ami.Raian <ami.robirai@gmail.com>
2024-08-30 02:35:38 +0600

    not-found page with next.js

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return(
+        <div className="h-screen flex justify-center items-center">
+            The requested resource was not found...!
+        </div>
+    )
+}
